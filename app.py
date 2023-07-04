b25f841d008a957c5a38a200f37f4111d7acb06b
screeck <screek666@gmail.com>
2023-07-04 16:24:45 +0200

    app.py fix

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/register.html')
+def home():
+    return render_template('register.html')
+
+@app.route('/login.html')
+def home():
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
