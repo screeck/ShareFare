a9d0a9bcab65bcb6343bb6bfc8e708c4b9ae5e25
screeck <screek666@gmail.com>
2023-07-04 23:58:02 +0200

    database query test 1

--- app.py
+++ app.py
@@ -1,14 +1,56 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request
+import psycopg2
 app = Flask(__name__)
+
+db_host = 'app-2b62167e-79b3-4a11-876c-ba8bc5ab7bb5-do-user-14289936-0.b.db.ondigitalocean.com'
+db_port = '25060'
+db_name = 'defaultdb'
+db_user = 'doadmin'
+db_password = 'AVNS_BlevRoe2edlQnO-TME1'
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/register')
-def register():
+# Route for displaying the registration form
+@app.route('/register', methods=['GET'])
+def display_registration_form():
     return render_template('register.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    # Retrieve form data from the request
+    first_name = request.form['firstName']
+    last_name = request.form['lastName']
+    email = request.form['email']
+    password = request.form['password']
+
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(
+        host=db_host,
+        port=db_port,
+        dbname=db_name,
+        user=db_user,
+        password=db_password,
+        sslmode='require'
+    )
+    cursor = conn.cursor()
+
+    # Insert the user data into the database
+    cursor.execute(
+        "INSERT INTO users (first_name, last_name, email, password) VALUES (%s, %s, %s, %s)",
+        (first_name, last_name, email, password)
+    )
+
+    # Commit the changes and close the connection
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+    # Redirect the user to a success page or perform any other necessary actions
+    return "User registered successfully!"
+
 
 @app.route('/login')
 def login():
