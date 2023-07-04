b1381813d01bda2f6586446b5cc1cd0699866f32
screeck <screek666@gmail.com>
2023-07-04 16:50:11 +0200

    changed href in index.html

--- app.py
+++ app.py
@@ -4,19 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('templates/index.html')
+    return render_template('index.html')
 
 @app.route('/register')
 def register():
-    return render_template('templates/register.html')
+    return render_template('register.html')
 
 @app.route('/login')
 def login():
-    return render_template('templates/login.html')
+    return render_template('login.html')
 
 @app.route('/main')
 def main():
-    return render_template('templates/main.html')
+    return render_template('main.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
